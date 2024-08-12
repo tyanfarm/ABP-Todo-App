02fef6fdc87be119286052ca4ca43103a4256d8f
tyanfarm <phamquangtuyen.nt@gmail.com>
2024-08-12 18:46:10 +0700

    init Order & Product Model

---
+++ aspnet-core/src/TodoApp.Domain/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace TodoApp
+{
+    public class Order : Entity<Guid>
+    {
+        public Guid ProductId { get; set; }
+        public Guid CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public Order()
+        {
+            OrderDate = DateTime.Now;
+        }
+    }
+}
---
+++ aspnet-core/src/TodoApp.Domain/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace TodoApp
+{
+    public class Product : Entity<Guid>
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
